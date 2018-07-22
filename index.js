7975a3739ef0945ef65f25cfc39bb0c4b33116f2
Liam Argent <liargent@hotmail.com>
2018-07-22 15:26:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your functions and code here
+function destructivelyAppendKitten(name) {
+  
+}
+
+function destructivelyPrependKitten(name) {
+  
+}
+
+function destructivelyRemoveLastKitten(name) {
+  
+}
+
+function destructivelyRemoveFirstKitten(name) {
+  
+}
+
+function appendKitten(name) {
+  
+}
+
+function prependKitten(name) {
+  
+}
+
+function removeLastKitten(name) {
+  
+}
