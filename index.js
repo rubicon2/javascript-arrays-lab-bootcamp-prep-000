f37467315f0b4abd51164644f634852b29865ef0
Liam Argent <liargent@hotmail.com>
2018-07-22 15:30:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // Add your functions and code here
-kittens = ["kitty"];
+kittens;
 
 function destructivelyAppendKitten(name) {
   kittens.push(name);
 }
 
 function destructivelyPrependKitten(name) {
-  
+  kittens.unshift(name);
 }
 
 function destructivelyRemoveLastKitten(name) {
